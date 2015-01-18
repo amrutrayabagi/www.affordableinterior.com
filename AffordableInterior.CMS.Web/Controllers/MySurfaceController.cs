872e776f6fa880a32332bb5cb24dc3c4b56e4c18
amrutrayabagi <amrut.bvb@gmail.com>
2015-01-17 23:21:31 -0800

    Following files are entered

---
+++ AffordableInterior.CMS.Web/Controllers/MySurfaceController.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Umbraco;
+
+namespace AffordableInterior.CMS.Web.Controllers
+{
+    public class MySurfaceController : Umbraco.Web.Mvc.SurfaceController
+    {
+        //
+        // GET: /MySurface/
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+    }
+}
